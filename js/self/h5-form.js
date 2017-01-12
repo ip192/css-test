e1213fcabf5c3ae18a2c2fad53a7c92ec539cffb
wp <1>
2017-01-12 09:35:23 +0800

    some js

---
+++ js/self/h5-form.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    var arr = [1, 2, '3', '4', 5];
+    var str_1 = 'abcdefg';
+    var str_2 = '12345';
+    var num = [1, 2, 3, 4, 5];
+
+    // map
+    // console.log(arr.map(String));
+    // console.log(arr.map(String).map(Number));
+
+    // console.log(num.map(pow));
+
+
+    // reduce
+    // console.log(arr.map(Number).reduce(function (a, b) {
+    //     return a + b;
+    // }));
+
+    // console.log('"12345" to 12345:', str_2.split('').map(Number).reduce(function (a, b) {
+    //     return a * 10 + b;
+    // }));
+
+    console.log(eval('["1","2","3","4"]').map(Number));
+
+    console.log(['aBc', 'Def', 'hiJ', 'KLm', 'nOP', 'QRS', 'tuv'].map(function (str) {
+        var reg = /(\b)[a-z](?=[a-z])*/;
+        return str.replace(reg, '#');
+    }));
+
+});
+
+function pow(x) {
+    return x*x;
+}
+
