4205c2bfd3621b24017be08eaff8456a8eca0731
wp <1>
2017-01-19 10:26:20 +0800

    delete react directory

--- react/index/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react-dom';
-import ReactDOM from 'react-dom';
-import MyConsole from '../components/console/MyConsole';
-
-
-ReactDOM.render(
-    <MyConsole />,
-    document.getElementById('css-test')
-);
