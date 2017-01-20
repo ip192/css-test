5a8e418a2d27c07872706f5270778496a72fdbf3
wp <1>
2017-01-20 20:45:06 +0800

    use pic rather than color for text background

--- original/js/self/file-read.js
+++ original/js/self/file-read.js
@@ -3,3 +3,7 @@ function readTxt() {
     var outputs = 'there\'s no server side';
     document.getElementsByClassName('output')[0].innerHTML = outputs;
 }
+
+$(document).ready(function () {
+    console.log('toString' in {});
+});
