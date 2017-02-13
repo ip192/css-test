144c0423fd4c37a7585cd16b294a4fb2ce2a6a7b
wp <1>
2017-02-13 16:38:08 +0800

    add some css3 test

--- original/js/self/some-css-test-1.js
+++ original/js/self/some-css-test-1.js
@@ -5,3 +5,14 @@ function showGeo() {
         console.log(err);
     });
 }
+
+// ready()
+(function () {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+        console.log('纬度', pos.coords.latitude, '经度', pos.coords.longitude);
+    }, function (err) {
+        console.log(err);
+    });
+    console.log(document.getElementsByClassName('outer-bottom')[0]);
+})();
+
